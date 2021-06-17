1678bc29c55f9344eca2a1f554bba032ea0a2a88
Aime Agustina Pelozo <aimeagustinagarces@gmail.com>
2021-06-17 19:53:35 -0300

    Mercado liebre con hamburgesa

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ const path = require("path")
 app.listen(3050, ()=>console.log("Running"))
 app.get("/", (req, res)=>res.sendFile(path.join(__dirname,"/views/home.html"))); // Primer barra es la raiz osea pagina principal 
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
